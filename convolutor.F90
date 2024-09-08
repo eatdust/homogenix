3f006f83f66cb5bb4fe4e6e36cd3dea3ec87a35e
chris <chris@ringeval.com>
2024-09-08 22:02:19 +0200

    Fix comments

--- convolutor.F90
+++ convolutor.F90
@@ -2,12 +2,12 @@
 !
 !   Copyright (C) 2024 C. Ringeval
 !   
-!   gaialaxy is free software: you can redistribute it and/or modify
+!   homogenix is free software: you can redistribute it and/or modify
 !   it under the terms of the GNU General Public License as published by
 !   the Free Software Foundation, either version 3 of the License, or
 !   (at your option) any later version.
 !
-!   gaialaxy is distributed in the hope that it will be useful,
+!   homogenix is distributed in the hope that it will be useful,
 !   but WITHOUT ANY WARRANTY; without even the implied warranty of
 !   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 !   GNU General Public License for more details.
